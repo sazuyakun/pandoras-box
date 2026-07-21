cf312e13030fdbfad72fbabf15705443602b598f
Soham Samal <sohamsamal37@gmail.com>
2026-07-22 00:30:07 +0530

    reading from .wav files using wavers

--- rust/wav-sheriff/self-notes/plan.md
+++ rust/wav-sheriff/self-notes/plan.md
@@ -1,7 +1,8 @@
 ## Steps
 x Args parsing
+x text files output
 - Pass files as arguments 
-  - takes files, outputs the name of the file
+  - read the file name for wav file
 - Make sure audio files can be passed
 - Able to plot audio files
   - research on representing it in experimentable format
--- rust/wav-sheriff/src/main.rs
+++ rust/wav-sheriff/src/main.rs
@@ -1,11 +1,14 @@
 use std::env;
 use std::fs;
+use wavers::{Wav, read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let contents = fs::read_to_string("files/test_1.txt").expect("Couldn't read files");
-    println!("{contents}");
+    let (samples, sample_rate) = read::<i16, _>("files/audio_1.wav").unwrap();
+
+    dbg!(&samples[100..150]);
+    dbg!(sample_rate);
 
     // let first_arg = &args[0];
     // let second_arg = &args[1];
