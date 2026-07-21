a0bad0f6a53db5f16a07abf4e91151c7a94485a5
Soham Samal <sohamsamal37@gmail.com>
2026-07-22 00:11:54 +0530

    reading from txt files

--- rust/wav-sheriff/src/main.rs
+++ rust/wav-sheriff/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let first_arg = &args[0];
-    let second_arg = &args[1];
-    println!("The first arg is {first_arg} and the second arg is {second_arg}");
+    let contents = fs::read_to_string("files/test_1.txt").expect("Couldn't read files");
+    println!("{contents}");
+
+    // let first_arg = &args[0];
+    // let second_arg = &args[1];
+    // println!("The first arg is {first_arg} and the second arg is {second_arg}");
 }
