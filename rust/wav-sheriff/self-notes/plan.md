8fa9b894e75a37e737fcb916237ffaf5c1704e0f
Soham Samal <sohamsamal37@gmail.com>
2026-07-22 00:34:32 +0530

    sampling from wav files

--- rust/wav-sheriff/self-notes/plan.md
+++ rust/wav-sheriff/self-notes/plan.md
@@ -1,9 +1,12 @@
 ## Steps
 x Args parsing
 x text files output
-- Pass files as arguments 
-  - read the file name for wav file
-- Make sure audio files can be passed
+x Pass files as arguments 
+x Make sure audio files can be passed
+
+### Audio files:
+- Understand samples and sample rate, if they can be used in some way
 - Able to plot audio files
   - research on representing it in experimentable format
+
 
--- rust/wav-sheriff/src/main.rs
+++ rust/wav-sheriff/src/main.rs
@@ -1,16 +1,12 @@
 use std::env;
-use std::fs;
-use wavers::{Wav, read};
+use wavers::read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (samples, sample_rate) = read::<i16, _>("files/audio_1.wav").unwrap();
+    let audio_file_path = &args[1];
+    let (samples, sample_rate) = read::<f32, _>(audio_file_path).unwrap();
 
-    dbg!(&samples[100..150]);
+    dbg!(&samples[100..110]);
     dbg!(sample_rate);
-
-    // let first_arg = &args[0];
-    // let second_arg = &args[1];
-    // println!("The first arg is {first_arg} and the second arg is {second_arg}");
 }
